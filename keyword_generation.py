743205c09f12973ac5b9b4b3c7abb0ef1f33b8c0
Beiqi Guan <beiqig@andrew.cmu.edu>
2018-04-18 16:23:49 -0700

    improved keyword generation

--- keyword_generation.py
+++ keyword_generation.py
@@ -3,6 +3,7 @@ from collections import OrderedDict
 
 desc = "Description\n Files 1\n Install Instructions\n All Comments 6\n\n\n\n\n\n\n\n\nHello S4 Modlings! I present to you my first upload: Tense Sense!\n\nThis Trait adds 4 unique moodlets which will both challenge and benefit affected Sims. They will randomly gain two powerful negative Moodlets, Tense Stress and Tense Anger, the former more frequently than the latter. If near Sims with which their Friendship is below 60% and Romance is below 30%, the Sim will gain Don't Want To Deal With Them, a powerful negative Moodlet. While in a neutral or positive Mood and free of the aforesaid Moodlets, Sims will gain a very powerful neutral Moodlet, Peace Of Mind.\n\nDue to their cumbersome social difficulties, they have no actual need for social interactions; their Social Need bar will never decrease. There are many other nit-picky details I've attached to all three Moodlets, viewable in the spoilers below.\n\n\n\n\nTense Stress\nTense +4, 2.5 hour duration.75% chance at 6-18 hour intervals.50% base chance on Moodlet addition to decrease Manners by 10, chance increased based on Trait Moodlets and negative Mood.While active:(Parenthood) All five Life Skills will decrease 25% more per interaction.Creative Skills increase 25% slower and are 1 rank less effective. (A skill at rank 6 will act as though it is rank 5)Mental Skills increase 30% slower and are 2 ranks less effective.Social Skills increase 50% slower and are 1 rank less effective.Musical Skills increase 20% slower and are 1 rank less effective.Physical and Athletic Skills increase 10% faster and are 1 rank more effective.Fun Need decays 10% faster.Hygiene Need decays 5% faster.Friendship will increase 10% slower and decrease 10% faster per interaction.Romance will increase 15% slower and decrease 15% faster per interaction.(Parenthood) Authority and Rivalry will increase 10% slower and decrease 10% faster per interaction.Sims are more likely to use Angry or negative autonomy interactions.\n\n\n\n\n\nTense Anger\nAngry +3, 2.5 hour duration.50% chance at 12-24 hour intervals.(Parenthood) 25% base chance on Moodlet addition to decrease Emotional Control by 10, chance increased based on Trait Moodlets and negative Mood.While active:(Parenthood) All five Life Skills will decrease 25% more per interaction.Creative Skills increase 25% slower and are 2 ranks less effective.Mental Skills increase 30% slower and are 2 ranks less effective.Social Skills increase 50% slower and are 2 ranks less effective.Musical Skills increase 20% slower and are 1 rank less effective.Physical and Athletic Skills increase 10% faster and are 2 ranks more effective.Fun Need decays 10% faster.Hygiene Need decays 5% faster.Friendship will increase 10% slower and decrease 10% faster per interaction.Romance will increase 15% slower and decrease 15% faster per interaction.(Parenthood) Authority and Rivalry will increase 10% slower and decrease 10% faster per interaction.Sims are more likely to use Angry or negative autonomy interactions.\n\n\n\n\n\nDon't Want To Deal With Them\n \n\nWhile Active:Proximity Buff; will activate when near any Sim(s) with less than 60% Friendship OR 30% Romance that is not part of your household.Continuous Staged Buff; Moodlet duration will continue to increase as long as you are in range of triggering Sims.60 minute grace period before triggering and adding the Moodlet, but Moodlet will persist until timer expires.If the Sim gets back in range of a trigger Sim, Moodlet will continue increasing again instantly. (No grace period until expiration)Moodlet has 9 Stages, each one Sim-hour; Moodlet increases 33% faster than it decreases. (40 Sim-minutes per Stage when increasing)Each Stage makes the Moodlet more intense; from Tense +1 to +6, then changes to Angry +4, +7, and finally +10.The Moodlet will stage downward, too! Each stage lasts 60 Sim-minutes, but is affected by anti-Tense interactions.Fun Need decays 10% faster.\n\n\n\n\n\nPeace Of Mind\nFine +3, Active Buff. Moodlet will appear any time the Sim is in a neutral/positive mood AND does not have any Trait debuff Moodlets.While Active:Increases Focused-Mood-based Careers' Performance gains by 50%.(Get To Work) Increases Scientist Breakthrough Progress gain by 33%.(Get To Work) Increases Detective Interrogation Table Progress gain by 25%.(Parenthood) Increases Life Skills gains by 33%.Increases all Mental Skill gains by ~17-33%.(Get Together) Increases Fitness and Dancing Skill gains by ~8%.(Cats & Dogs, Vampires)Increases Pet Training and Vampire Lore Skill gains by ~17%.(Get To Work) Increases Retail Skill gains by 25%.Increases Hack Payout and Homework gains by ~17%.Increases Crafting Progress gains by 25%.(Cats & Dogs) Increases Discipline gains by 25%.Increases all Programming crafts progress gains by 25%.Increases Horseshoes and Video Games' Skills by ~17%.Increases Woodwork Progress gain by 25%.\n\n\nAdditional Credits:\nThanks are almost entirely to:\n\nZerbu's Mod Constructor\nSims 4 Studio\ns4pe\n\n\n\n\n\nKey:  - File was updated after upload was posted\n\n\n\n\nFilename - Tip: You can click the magnifying glass to see the archive contents\nSize\nDownloads\nDate\n\n\n\n\n\n\n\n\n\nKarazhan87_TenseTrait.7z\n| Description: Sims with the Tense Sense trait have a Tense personality! They will become Tense and/or Angry at random intervals, but benefit from a clear mind.\n\n\n22.2 KB\n1,285\n23rd Jan 2018 \n\n\n\n\n\n17\nThanks\n\nSay Thanks\n\n\n\nNeed help with downloading or installing content? Click this link!\n\n\n\n\n\n\n\n\n Basic Download and Install Instructions:1. Download: Click the File tab to see the download link. Click the link to save the .rar or .zip file(s) to your computer. Read the upload description for installation instructions. If nothing else is specified, do the following:  No Extraction Required (Script Mods):1. Cut and paste the .zip file(s) into your Mods folderWindows XP: Documents and Settings\\(Current User Account)\\My Documents\\Electronic Arts\\The Sims 4\\Mods\\Windows Vista/7/8/8.1: Users\\(Current User Account)\\Documents\\Electronic Arts\\The Sims 4\\Mods\\Script mods need to be placed directly in \\Mods or one level below that (e.g. \\Mods\\Scripts)  sub-folders nested any deeper wont work.\n  Extraction Required (Everything Else):1. Use WinRAR (Windows) to extract the .package file(s) from the .rar or .zip file(s).2. Cut and paste the .package file(s) into your Mods folderWindows XP: Documents and Settings\\(Current User Account)\\My Documents\\Electronic Arts\\The Sims 4\\Mods\\Windows Vista/7/8/8.1: Users\\(Current User Account)\\Documents\\Electronic Arts\\The Sims 4\\Mods\\\n\n Need more information?Anyone can use both .rar and .zip files easily! On Windows, use WinRAR.If you don't have a Mods folder, just make one.Mod still not working? Make sure you have script mods enabled\n\n\n\n\n\n\n\n\n\n\n\n \n\n\n\n\n\n\n\n\n\nSearch\nAdvanced Search\n\n\n\n\nBack to download view\n\n\n6 Comments / Replies (Who?)\n- 4 Feedback Posts,\n1 Thanks Posts\nHide Thanks Posts for this thread (Show only feedback)"
 stop_words = set();
+final_ngrams = {};
 
 def split_desc_to_words(desc):
   res = "";
@@ -12,6 +13,15 @@ def split_desc_to_words(desc):
   res = res.lower();
 
   return res.split(" ");
+
+def clean_word(word):
+  res = "";
+  res = re.sub(r"(([^a-zA-Z])+(')([^a-zA-Z])+|([^a-zA-Z])+(')|(')([^a-zA-Z])+|[^a-zA-Z']+)", " ", desc);
+  res = re.sub(r"\\s+", " ", res);
+  res = re.sub(r"(^(\\s+)|(\\s+)$)", "", res);
+  res = res.lower();
+
+  return res;
 
 # read from in put and build a set of stopwords
 def build_stop_words_set():
@@ -24,9 +34,8 @@ def build_stop_words_set():
   src.close()
   return;
 
-def generate_ngram_keywords(keywords):
+def generate_ngram_keywords(keywords, mult):
   ngram_words = {};
-  final_ngrams = {};
 
   # one gram
   for i in range(0, len(keywords)):
@@ -41,9 +50,9 @@ def generate_ngram_keywords(keywords):
         continue;
 
     if (keywords[i] in ngram_words and keywords[i] not in stop_words):
-      ngram_words[keywords[i]] += 1;
+      ngram_words[keywords[i]] += mult;
     else:
-      ngram_words[keywords[i]] = 1;
+      ngram_words[keywords[i]] = mult;
 
   # two gram
   for i in range(0, len(keywords)-1):
@@ -58,9 +67,9 @@ def generate_ngram_keywords(keywords):
       continue;
 
     if (two_gram_word in ngram_words):
-      ngram_words[two_gram_word] += 1;
+      ngram_words[two_gram_word] += mult;
     else:
-      ngram_words[two_gram_word] = 1;
+      ngram_words[two_gram_word] = mult;
 
 
   # three gram
@@ -76,29 +85,61 @@ def generate_ngram_keywords(keywords):
       continue;
 
     if (three_gram_word in ngram_words):
-      ngram_words[three_gram_word] += 1;
+      ngram_words[three_gram_word] += mult;
     else:
-      ngram_words[three_gram_word] = 1;
+      ngram_words[three_gram_word] = mult;
 
   # remove the ones that only appeared 1 time
   for key, value in ngram_words.items():
     if value >= 2:
       final_ngrams[key] = value;
 
+  return; 
+
+def generate_ngram_keywords_for_doc(doc):
+  if (len(stop_words) == 0):
+    build_stop_words_set();
+
+  generate_ngram_keywords_from_desc(doc['description']);
+  generate_ngram_keywords_from_title(doc['title']);
+  generate_ngram_keywords_from_word_array(doc['tags']);
+  generate_ngram_keywords_from_word_array(doc['types']);
+
   final_ngrams = OrderedDict(sorted(final_ngrams.items(), key=lambda x : x[1], reverse=True));
 
-  return final_ngrams; 
+  return final_ngrams;
 
 def generate_ngram_keywords_from_desc(desc):
   if (len(stop_words) == 0):
     build_stop_words_set();
 
   words = split_desc_to_words(desc);
-  final_ngram_keywords = generate_ngram_keywords(words);
+  generate_ngram_keywords(words, 1);
 
-  return final_ngram_keywords;
+  return;
+
+def generate_ngram_keywords_from_title(title):
+  if (len(stop_words) == 0):
+    build_stop_words_set();
+
+  words = split_desc_to_words(title);
+  generate_ngram_keywords(words, 5);
+
+  return;
+
+def generate_ngram_keywords_from_word_array(types):
+  if (len(stop_words) == 0):
+    build_stop_words_set();
+
+  words = [];
+  for t in types:
+    words.append(clean_word(t));
+
+  generate_ngram_keywords(words, 5);
+
+  return;
 
 if __name__ == "__main__":
   build_stop_words_set();
   keywords = split_desc_to_words(desc);
-  ngram_keywords = generate_ngram_keywords(keywords);
+  generate_ngram_keywords(keywords, 1);
--- regenerate_keywords.py
+++ regenerate_keywords.py
@@ -7,13 +7,15 @@ from keyword_generation import generate_ngram_keywords_from_desc;
 client = MongoClient('localhost', 27017);
 
 db = client.sims_test_db;
-collection = db.sims_new;
+collection = db.sims_records_test;
 item_count = 0;
 
 
 def regenerate_keyword_for_doc(document):
   if ('description' in document):
-    document['keywords'] = generate_ngram_keywords_from_desc(document['description']);
+    new_keywords = generate_ngram_keywords_for_doc(document);
+    document['keywords'] = new_keywords;
+
     collection.update_one({'_id': document['_id']}, {"$set": document}, upsert=True);
 
 
