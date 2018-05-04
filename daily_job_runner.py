03a4f0a519df60aec707f97cc47e8c5941198c96
Beiqi Guan <beiqig@andrew.cmu.edu>
2018-05-04 00:35:09 -0700

    incorporated aggregation and mapred operations to daily job runner

--- daily_job_runner.py
+++ daily_job_runner.py
@@ -1,10 +1,12 @@
 import schedule
 import time
 import modthesims_scraper
+import os
 
 def job():
   print('Running daily job...');
   modthesims_scraper.start_scraping();
+  os.system("cd db_operations && ./db_op.sh")
 
 if __name__ == "__main__":
   print('Starting...');
